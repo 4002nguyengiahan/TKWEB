7657fc3b83ea19df19bb191199f075c906fce421
nguyễn gia hân <117701732+4002nguyengiahan@users.noreply.github.com>
2023-05-26 13:48:25 +0700

    Update JS01.js

--- BÀI 3/JS01.js
+++ BÀI 3/JS01.js
@@ -1,10 +1,10 @@
 
 function kiemTraChanLe(so) {
   if (so % 2 == 0) {
-    console.log("Day la so chan");
+    consochan.log("Day la so chan");
   } else console.log("Day la so le");
 }
 
 // Ví dụ sử dụng
 console.log(kiemTraChanLe(7)); // Kết quả: "Lẻ"
-console.log(kiemTraChanLe(10)); // Kết quả: "Chẵn"
+consochan.log(kiemTraChanLe(10)); // Kết quả: "Chẵn"
